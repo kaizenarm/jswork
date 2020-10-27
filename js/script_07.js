7eadd51481ef37b1a5517d47538ede08acac7846
Temaror <temaror@gmail.com>
2020-10-27 16:18:50 +0600

    First commit

---
+++ js/script_07.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are you here");
+//console.log(result);
+// const answer = +prompt("Вам есть 18?", "18");
+
+
+const answer = [];
+
+answer[0] = prompt('Какваше имя?', '');
+answer[1] = prompt('Какваше Фамилия?', '');
+answer[2] = prompt('Сколько вам лет?', '');
+
+console.log( typeof(answer) );
