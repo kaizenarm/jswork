bd07f53f5cc8fd17a649b539a96caa4ac7f80c61
Temaror <temaror@gmail.com>
2020-10-30 12:12:53 +0600

    Теория про функции 3 вида function declaratiom, function expression, стрелочные функции

--- js/script.js
+++ js/script.js
@@ -1,46 +1,19 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?','');
+let num = 20;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-
-for ( let i = 0; i < 2; i++ ){
-    const a = prompt('Один из последних просмотренных фильмов?',''),
-          b = prompt('На сколько оцените его?','');
-    if ( a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ){
-        personalMovieDB.movies[a] = b;
-        console.log('Done');
-    }else{
-        i--;
-        console.log('Error');
-    }
-    
+function showFirstMessage(text){
+    console.log(text);
+    num = 20;
 }
 
-if ( personalMovieDB.count < 10){
-    alert('Просмотрено довольно мало фильмов');
-} else if( personalMovieDB.count >= 10 && personalMovieDB.count < 30 ){
-    alert('Вы классический зритель');
-}else if ( personalMovieDB.count >= 30 ){
-    alert('Вы киноман');
-}else{
-    alert('Произошла ошибка');
+showFirstMessage('Hello World!');
+console.log(num);
+
+function calc(a, b){
+    return (a + b);
 }
 
-
-// const a = prompt('Один из последних просмотренных фильмов?',''),
-//       b = prompt('На сколько оцените его?',''),
-//       c = prompt('Один из последних просмотренных фильмов?',''),
-//       d = prompt('На сколько оцените его?','');
-
-// personalMovieDB.movies[a] = b;
-// personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 6));
