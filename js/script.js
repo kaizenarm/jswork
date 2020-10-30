943c893f8eab8eee746adef141f740f1ba3fd55b
Temaror <temaror@gmail.com>
2020-10-30 16:00:16 +0600

    Виды свойств и методы

--- js/script.js
+++ js/script.js
@@ -1,19 +1,26 @@
 'use strict';
 
-let num = 20;
+const str = "Test";
 
-function showFirstMessage(text){
-    console.log(text);
-    num = 20;
-}
+//const arr = [1,2,4]
 
-showFirstMessage('Hello World!');
-console.log(num);
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
 
-function calc(a, b){
-    return (a + b);
-}
+const fruit = "Some fruit";
 
-console.log(calc(4, 3));
-console.log(calc(5, 6));
-console.log(calc(10, 6));
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
