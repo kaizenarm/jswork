dcab0be93365397db010d69a47655dc426e85b88
Temaror <temaror@gmail.com>
2020-11-01 20:25:14 +0600

    Обьекты, деструктуризация обьектов

--- js/script.js
+++ js/script.js
@@ -1,26 +1,46 @@
 'use strict';
 
-function first(){
-    //Do something
-    setTimeout(function(){
-        console.log(1);
-    }, 500);
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function(){
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+
+console.log(border);
+
+//console.log(Object.keys(options).length);
+
+//console.log(options['colors']['border']);
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+let counter = 0;
+
+for (let key in options){
+    if( typeof(options[key]) === 'object'  ){
+        for (let i in options[key]){
+            console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Свойство ${key} имеет значение ${options[key]}`);
+        counter++;
+    }
+    
 }
-
-function second(){
-    console.log(2);
-}
-
-first();
-second();
-
-function learnJS(lang, callback){
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-
-function done(){
-    console.log('Я прошел этот урок');
-}
-
-learnJS('JavaScript', done);
+console.log(counter);
