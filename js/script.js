36428e9b7d08e9bcc4cf5771d9bb64eb5169eec6
Temaror <temaror@gmail.com>
2020-11-06 17:48:28 +0600

    Получение элементов со страницы DOM documentobject model

--- js/script.js
+++ js/script.js
@@ -1,33 +1,20 @@
 "use strict";
 
-// let x = 5; 
-// alert( x++ ); 
+const box = document.getElementById('box');
 
-// console.log([] + false - null + true ); // NaN
+console.log(box);
 
-// let y = 1; 
-// let x = y = 2; 
-// alert(x);  // 2
+const buttons = document.getElementsByTagName('button');
+console.log(buttons[0]);
 
-// console.log([] + 1 + 2); //12
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
 
-// alert( "1"[0] ); //1
+const hearts = document.querySelectorAll('.heart');
 
-// console.log(2 && 1 && null && 0 && undefined ); // null
+hearts.forEach(item=>{
+    console.log(item);
+});
 
-// console.log(!!( 1 && 2 ) === (1 && 2)); //false !! это булиновое 
-
-// alert( null || 2 && 3 || 4 );   // 3
-
-// И запинается на лжи
-// ИЛИ запинается на правде
-
-// const a = [1, 2, 3]; 
-// const b = [1, 2, 3]; 
-// console.log(a == b); //false
-
-// alert( +"Infinity" ); // Infinity - number число
-
-// console.log("Ёжик" > "яблоко" ); // false
-
-console.log( 0 || "" || 2 || undefined || true || false );
+const oneHeart = document.querySelector('div');
+console.log(oneHeart);
