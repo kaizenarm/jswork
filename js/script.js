67aabdff1acd145c19dea901db5b1a751d43889e
Temaror <temaror@gmail.com>
2020-11-07 16:16:38 +0600

    Навигация по DOM - элементам, data-аттрибуты, преимущества for of

--- js/script.js
+++ js/script.js
@@ -1,56 +1,24 @@
 "use strict";
 
-const box = document.getElementById('box'),
-      buttons = document.getElementsByTagName('button'),
-      circles = document.getElementsByClassName('circle'),
-      wrapper = document.querySelector('.wrapper'),
-      hearts = wrapper.querySelectorAll('.heart'),
-      oneHeart = wrapper.querySelector('.heart');
+// console.log(document.body);
+// console.log(document.head);
+// console.log(document.documentElement);
+// console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.firstElementChild);
+// console.log(document.body.lastChild);
 
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-// box.style.backgroundColor = 'blue';
-// box.style.width = '500px';
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
 
-box.style.cssText = 'background-color: blue; width: 500px';
+for (let node of document.body.childNodes){
+    if(node.nodeName == '#text'){
+        continue;
+    }
 
-buttons[1].style.borderRadius = '100%';
-circles[0].style.backgroundColor = 'red';
-
-// for( let i = 0; i < hearts.length; i++){
-//     hearts[i].style.backgroundColor = 'blue';
-// }
-
-hearts.forEach(item =>{
-    item.style.backgroundColor = 'blue';
-});
-
-const div = document.createElement('div');
-// const text = document.createTextNode('Тут был я');
-
-div.classList.add('black');
-
-// document.body.append(div);
-// wrapper.append(div);
-// wrapper.appendChild(div);
-
-wrapper.prepend(div);
-
-// hearts[0].before(div);//новое
-// hearts[0].after(div);//новое
-
-// wrapper.insertBefore(div, hearts[1]);//старое
-
-// circles[0].remove();//новое
-// wrapper.removeChild(hearts[1]);//старое
-
-hearts[0].replaceWith(circles[0]); //новое
-// wrapper.replaceChild(circles[0], hearts[0]); //старое
-
-div.innerHTML = "<h1>Hello world</h1>"; // HTML тексты
-
-// div.textContent = "Hello"; //только текст
-
-// div.insertAdjacentHTML("beforebegin", '<h2>Hello</h2>');
-// div.insertAdjacentHTML("afterbegin", '<h2>Hello</h2>');
-// div.insertAdjacentHTML("beforeend", '<h2>Hello</h2>');
-div.insertAdjacentHTML("afterend", '<h2>Hello</h2>');
+    console.log(node);
+}
