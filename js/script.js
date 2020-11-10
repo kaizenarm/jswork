1bc23a67e4ec9a9397b80f351745bb17d834c8f8
Temaror <temaror@gmail.com>
2020-11-10 11:08:58 +0600

    037 - ClassList и делегирование событий

--- js/script.js
+++ js/script.js
@@ -1,14 +1,48 @@
 "use strict";
 
-const p = document.querySelectorAll('p');
-console.log(p);
+const btns = document.querySelectorAll('button'),
+      wrapper = document.querySelector('.btn-block');
 
-function loadScript(src){
-    const script = document.createElement('script');
-    script.src = src;
-    script.async = false;
-    document.body.append(script);
-}
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(1));
+// console.log(btns[1].classList.add('red','dfgsdg'));
+// console.log(btns[0].classList.remove('blue'));
+// console.log(btns[0].classList.toggle('blue'));
 
-loadScript("js/test.js");
-loadScript("js/some.js");
+// if (btns[2].classList.contains('red')){
+//     console.log('red');
+// }
+
+btns[0].addEventListener('click', () => {
+    // if (!btns[1].classList.contains('red')){
+    //     btns[1].classList.add('red');
+    // }else{
+    //     btns[1].classList.remove('red');
+    // }
+
+    btns[1].classList.toggle('red');
+});
+
+// console.log(btns[0].className);
+
+wrapper.addEventListener('click', (event) => {
+    // if (event.target && event.target.classList.contains('blue')) {
+    //     console.log("Hello");
+    // }
+    // if (event.target && event.target.tagName == "BUTTON") {
+    //     console.log("Hello");
+    // }
+    if (event.target && event.target.matches("button.red")) {
+        console.log("Hello");
+    }
+});
+
+// btns.forEach(btn => {
+//     btn.addEventListener('click', () => {
+//         console.log('Hello');
+//     });
+// });
+
+const btn = document.createElement('button');
+btn.classList.add('red');
+wrapper.append(btn);
