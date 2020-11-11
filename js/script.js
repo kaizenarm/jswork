85d737049384b7a05612ac249ebd298aad6dcead
Temaror <temaror@gmail.com>
2020-11-11 11:48:22 +0600

    040 - Работа с датами

--- js/script.js
+++ js/script.js
@@ -1,40 +1,31 @@
-const btn = document.querySelector('.btn');
-let timerId,
-    i = 0;
+'use strict';
 
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let pos = 0;
+const now = new Date('2020-11-11');
 
-    const id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 300) {
-            clearInterval(id);
-        } else {
-            pos++;
-            elem.style.top = pos + "px";
-            elem.style.left = pos + "px";
-        }
-    }
-}    
+// new Date.parse('2020-11-11');
 
-btn.addEventListener('click', myAnimation);
-
-// btn.addEventListener('click', () => {
-//     const timerId = setTimeout(logger, 2000);
-//     // timerId = setInterval(logger, 500);
-// });
+console.log(now.setHours(18, 40));
+console.log(now);
 
 
-// function logger () {
-//     if (i === 3) {
-//         clearInterval(timerId);
-//     }
-//     console.log('text');
-//     i++;
-// }
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getDay());
 
-// let id = setTimeout(function log (){
-//     console.log('Hello');
-//     id = setTimeout(log, 500);
-// }, 500);
+// console.log(now.getUTCHours());
+
+// console.log(now.getTimezoneOffset());
+console.log(now.getTime());
+
+let start = new Date();
+
+for (let i = 0; i < 100000; i++){
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисикунд`);
