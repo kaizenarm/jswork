250dba1990d9176254fb6336b482e2a8b647c973
Temaror <temaror@gmail.com>
2020-11-13 21:22:57 +0600

    051 JSON формат передачи данных, глубокое клонирование обьектов

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 "use strict";
 
-const log = function(a, b, ...rest) {
-    console.log(a, b, rest);
+const persone = {
+    name: 'alex',
+    tel: '+74444444',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
 };
 
-log('basic', 'rest', 'operator', 'usage');
+const clone = JSON.parse(JSON.stringify(persone));
 
-function calcOrDouble(number, basis = 2) {
-    // basis = basis || 2;
-    console.log(number * basis);
-}
-
-calcOrDouble(3);
+clone.parents.mom = "Ann";
+console.log(persone);
+console.log(clone);
