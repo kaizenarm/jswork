ee2aca822131bf5173a4e65322d6612df7b2ab82
Temaror <temaror@gmail.com>
2020-11-13 09:50:11 +0600

    049 - Rest оператор и параметры по умолчанию (ES6)

--- js/script.js
+++ js/script.js
@@ -1,36 +1,14 @@
 "use strict";
 
-class Rectangle {
-    constructor(height, width){
-        this.height = height;
-        this.width = width;
-    }
+const log = function(a, b, ...rest) {
+    console.log(a, b, rest);
+};
 
-    calcArea() {
-        return this.height * this.width;
-    }
+log('basic', 'rest', 'operator', 'usage');
+
+function calcOrDouble(number, basis = 2) {
+    // basis = basis || 2;
+    console.log(number * basis);
 }
 
-class ColoredRectangleWithText extends Rectangle {
-    constructor(height, width, text, bagColor){
-        super(height, width);
-        this.text = text;
-        this.bagColor = bagColor;
-    }
-
-    showMyProps() {
-        console.log(`Текст: ${this.text}, цвет: ${this.bagColor}`);
-    }
-}
-
-const div = new ColoredRectangleWithText(25,10, 'Hello World', 'red');
-
-div.showMyProps();
-console.log(div.calcArea());
-
-// const square = new Rectangle(10,10);
-// const long = new Rectangle(20,100);
-
-// console.log(long.calcArea());
-// console.log(square.calcArea());
-
+calcOrDouble(3);
