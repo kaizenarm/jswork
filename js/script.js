37669cbfe0a74ddfa63b466ef7bec12d35c9d3b5
Temaror <temaror@gmail.com>
2020-11-18 18:57:53 +0600

    058 - одробно про npm и проект JSON сервер

--- js/script.js
+++ js/script.js
@@ -26,3 +26,29 @@ const some = [4, 7, 9];
 // console.log(some.some(item => typeof(item) === 'number'));
 
 console.log(some.every(item => typeof(item) === 'number'));
+
+// reduce
+
+const arr = [4, 5, 1, 3, 2, 6];
+
+const res = arr.reduce((sum, current) => sum + current, 3);
+
+console.log(res);
+
+// const arr = ['apple', 'pear', 'plum'];
+
+// const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+// console.log(res);
+
+const obj = {
+    ivan: 'persone',
+    ann: 'persone',
+    dog: 'animal',
+    cat: 'animal'
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+console.log(newArr);
