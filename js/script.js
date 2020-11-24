4d149117748c6522095bb77140fea74457e27aab
Temaror <temaror@gmail.com>
2020-11-24 12:20:14 +0600

    068 - Геттеры и сетеры (свойства обьектов)

--- js/script.js
+++ js/script.js
@@ -1,35 +1,17 @@
 "use strict";
 
-// new RegExp('pattern', 'flags');
-// /pattern/f
+const persone = {
+    name: 'Alex',
+    age: 25,
 
-// const ans = prompt('Введите ваше число');
+    get userAge() {
+        return this.age;
+    },
 
-// const reg = /\d/g;
-// console.log(ans.match(reg));
+    set userAge(num){
+        this.age = num;
+    }
+};
 
-const str = 'My name is R2D2';
-
-console.log(str.match(/\D/ig));
-
-// \D - не числа
-// \W - не буквы
-// \S - не 
-
-// \d - мы ищем цифры
-// \w - мы ищем все слова и буквы 
-// \s - мы ищем все пробелы
-
-// i - вне зависимости от регистров
-// g - пытаемся найти несколько значений, то есть все
-// m - многострочный режим
-
-// console.log(ans.search(reg));
-// console.log(ans.match(reg));
-
-// const pass = prompt('Password');
-
-// console.log(pass.replace(/./g, "*"));
-
-// console.log('12-34-56'.replace(/-/g, ':'));
-
+console.log(persone.userAge = 30);
+console.log(persone.userAge);
