91d789d2b8606a6ec7e888bd9367f6be60171017
Temaror <temaror@gmail.com>
2020-11-24 15:54:32 +0600

    070 - Прием модуль, как и зачем его использовать

--- js/script.js
+++ js/script.js
@@ -1,30 +1,23 @@
 "use strict";
 
-class User {
-    constructor(name, age){
-        this.name = name;
-        this._age = age;
+const number = 1;
+
+(function(){
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
+
+console.log(number);
+
+const user = (function(){
+    const privat = function() {
+        console.log('I am privat!');
+    };
+
+    return {
+        say: privat
     }
+}());
 
-    #surname = 'Terrrr';
-
-    say = () => {
-        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возвраст ${this._age}`);
-    }
-
-    get age() {
-        return this._age;
-    }
-
-    set age(age) {
-        if(typeof age === 'number' && age > 0 && age < 110){
-            this._age = age;
-        }else{
-            console.log('Недопустимое значение');
-        }
-    }
-}
-
-const ivan = new User('Ivan', 27);
-console.log(ivan.surnam);
-ivan.say();
+user.say();
