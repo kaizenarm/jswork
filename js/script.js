1ef7aabbfaaf4a3d58e246865706110f4b0734af
Temaror <temaror@gmail.com>
2020-11-27 10:40:35 +0600

    079 - Функции генераторы

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
-// Angular:
-//  - node.js
-//  - TypeScript
-//  - webpack
-//  - MVC pattern
-//  - Angular
- 
-// React:
-//  - JSX (препроцессор)
-//  - Babel
-//  - Webpack
-//  - React
-//  - node.js
+'use strict';
 
-// Vue.js:
-//  - Webpack
-//  - Vue
-//  - node.js
+// function* generator() {
+//     yield 's';
+//     yield 'c';
+//     yield 'r';
+//     yield 'i';
+//     yield 'p';
+//     yield 't';
+// }
+
+// const str = generator();
+
+// console.log(str.next().value);
+
+function* count(n) {
+    for (let i = 0; i < n; i++){
+        yield i;
+    }
+}
+
+const num = count(7);
+
+// console.log(num.next().value);
+
+for (let n of num){
+    console.log(n.next().value);
+}
+
